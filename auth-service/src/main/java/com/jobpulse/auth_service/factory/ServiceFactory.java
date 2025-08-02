da939baa4f5680ed01da207ac0233993988a9540
info-dpv-intelligence <info@dpv-intelligence.com>
2025-08-02 10:54:33 +0200

    refactor(auth-service): removed old junk

--- auth-service/src/main/java/com/jobpulse/auth_service/factory/ServiceFactory.java
+++
@@ -1,9 +0,0 @@
-package com.jobpulse.auth_service.factory;
-
-import com.jobpulse.auth_service.service.module.password.PasswordServiceContract;
-import com.jobpulse.auth_service.service.module.jwt.JwtServiceContract;
-
-public interface ServiceFactory {
-    PasswordServiceContract createPasswordService();
-    JwtServiceContract createJwtService();
-}
